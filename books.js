f26f06490247c4173c72e83286f1a208763aad19
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-07-02 23:37:09 -0300

    add ability to add and remove books

---
+++ books.js
@@ -0,0 +1,42 @@
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const addBtn = document.querySelector('#addButton');
+const booksDiv = document.querySelector('#booksDiv');
+let books = [{
+  title : "Stormligth Archive",
+  author : "Brandon Sanderson"
+}]
+
+function book(title, author){
+  this.title = title;
+  this.author = author;
+}
+
+function createBook(){
+  let newBook = new book(titleInput.value,authorInput.value);
+  books.push(newBook);
+  display(newBook);
+  titleInput.value = "";
+  authorInput.value = "";
+}
+function display(obj){
+  let aBook = document.createElement('div');
+  aBook.id = "a"+Date.now();
+  let aBookId = aBook.id;
+  let buttonId = "a"+Date.now();
+  aBook.innerHTML = `
+  <p>${obj.title}</p>
+  <p>${obj.author}</p>
+  <button id="${buttonId}">Remove</button>
+  `;
+  booksDiv.appendChild(aBook);
+  let removeBtn = document.querySelector('#'+buttonId);
+  removeBtn.addEventListener('click',(evt) =>{evt.stopPropagation();removeBook(aBookId,obj);} );
+}
+function removeBook(bookId,obj){
+  books.splice(books.indexOf(obj),1)
+  const bookToRemove = document.querySelector('#'+bookId)
+  booksDiv.removeChild(bookToRemove);
+}
+window.onload = books.forEach(display)
+addBtn.addEventListener('click',createBook);
